06fb587a72187c8f5929d4bb13a891e42e26840c
VlBl <vladpuku@gmail.com>
2025-03-25 19:27:44 +0200

    Started doing coding

--- app/src/main/java/com/example/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -2,7 +2,8 @@ package com.example.currencyconverter
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
+import android.view.View
+import android.widget.AdapterView
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -13,10 +14,17 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.currencyconverter.databinding.ActivityMainBinding
 
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private var valuesFirst = arrayOf("USD", "UAH")
+    private var valuesSecond = arrayOf("UAH", "USD")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +34,46 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Бліняєв В.О.", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
+        val firstSpinner: Spinner = binding.appBarMain.firstCurrency;
+        val secondSpinner: Spinner = binding.appBarMain.secondCurrency;
+
+        val adapterFirst = ArrayAdapter(this, android.R.layout.simple_spinner_item, valuesFirst)
+        adapterFirst.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        val adapterSecond = ArrayAdapter(this, android.R.layout.simple_spinner_item, valuesSecond)
+        adapterSecond.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        firstSpinner.adapter = adapterFirst
+        secondSpinner.adapter = adapterSecond
+
+        firstSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedItem = parent.getItemAtPosition(position).toString()
+                Toast.makeText(this@MainActivity, "Selected: $selectedItem", Toast.LENGTH_SHORT).show()
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        };
+        secondSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val firstSelectedItem = firstSpinner.selectedItem.toString()
+                val selectedItem = parent.getItemAtPosition(position).toString()
+                if (selectedItem == firstSelectedItem) {
+                    Toast.makeText(this@MainActivity, "NOOOO", Toast.LENGTH_SHORT).show()
+                    secondSpinner.setSelection(0)
+                }
+                else {
+                    Toast.makeText(this@MainActivity, "Selected: $selectedItem", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        };
+
+        binding.appBarMain.convertbutton.setOnClickListener { view ->
+            val input = binding.appBarMain.input.text.toString()
+            binding.appBarMain.output.text = input
         }
+
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
